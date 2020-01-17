cb0078a8727e258f59e79ff4a05583cafdf545d8
Etienne Prothon <etienne.prothon@exwexs.fr>
2020-01-17 14:47:19 +0100

    fix: Using web::block it work

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ node canon.js
 
 # Observation
 Lot of 408 errors when a set of request end (for exemple the first 8 request finish, we will get 200 for the 8 and some 408 for others)
+
+# Fix
+Using web::block work because we are not blocking the actix thread and we move heavy computing in another thread.  
+I let this repo if someone want to check!
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ extern crate log;
 
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 use actix_web::middleware::Logger as actix_Logger;
-use std::thread;
-use std::time::{Duration};
 use std::env;
 
 extern crate slog_envlogger;
@@ -13,13 +11,41 @@ extern crate rand;
 
 use rand::Rng;
 
+const SIZE: usize = 100_000_000;
+
+fn heavy_process() -> Result<(), ()> {
+    // let mut sub_results = vec![0.0f64; SIZE];
+    let mut divisor = 1.0;
+    let mut result: f64;
+    let mut tmp = 0.0;
+
+    for i in 0..SIZE {
+        tmp = 4.0 / divisor;
+        divisor += 2.0;
+    }
+
+    result = 0.0;
+
+    for i in 0..SIZE {
+        if i % 2 == 0 {
+            result += tmp;
+        }
+        else {
+            result -= tmp;
+        }
+    }
+    info!("{}", result);
+    Ok(())
+    // thread::sleep(Duration::from_secs(5));
+}
+
 async fn greet(req: HttpRequest) -> impl Responder {
     let mut rng = rand::thread_rng();
     let n: u8 = rng.gen();
     info!("Taking request {}", n);
     let name = req.match_info().get("name").unwrap_or("World");
-    thread::sleep(Duration::from_secs(5));
     info!("Answering {}", n);
+    web::block(heavy_process).await.expect("FAIL");
     format!("Hello {}!", &name)
 }
 
